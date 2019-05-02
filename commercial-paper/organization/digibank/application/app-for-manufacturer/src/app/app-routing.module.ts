99c9e548263defb0bc8f3a9393d4f1c31ae59d6b
yunchen <chen.yunc@husky.neu.edu>
2019-05-02 06:35:07 +0200

    Updated ui dashboard

--- commercial-paper/organization/digibank/application/app-for-manufacturer/src/app/app-routing.module.ts
+++ commercial-paper/organization/digibank/application/app-for-manufacturer/src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LogisticsMgComponent } from './logistics-mg/logistics-mg.component';
+import { ProcessMgComponent } from './process-mg/process-mg.component';
+import { ProcurementMgComponent } from './procurement-mg/procurement-mg.component';
+import { ProductMgComponent } from './product-mg/product-mg.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  // { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'logistics', component: LogisticsMgComponent },
+  { path: 'process', component: ProcessMgComponent },
+  { path: 'procurement', component: ProcurementMgComponent },
+  { path: 'product', component: ProductMgComponent }
+];
 
 @NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
 }) 
 export class AppRoutingModule {}
--- commercial-paper/organization/digibank/application/app-for-manufacturer/src/app/app.module.ts
+++ commercial-paper/organization/digibank/application/app-for-manufacturer/src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from "@angular/material/icon";
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +14,10 @@ import { ProcessMgComponent } from './process-mg/process-mg.component';
 import { ProductMgComponent } from './product-mg/product-mg.component';
 import { ProcurementMgComponent } from './procurement-mg/procurement-mg.component';
 import { LogisticsMgComponent } from './logistics-mg/logistics-mg.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
@@ -17,12 +26,20 @@ import { LogisticsMgComponent } from './logistics-mg/logistics-mg.component';
     ProcessMgComponent,
     ProductMgComponent,
     ProcurementMgComponent,
-    LogisticsMgComponent
+    LogisticsMgComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
